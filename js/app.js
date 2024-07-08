8a81eae3b4b4f02fc60ba27c5579db462619e0b6
Maryam Qasim <maryam@DESKTOP-6B29GKT.>
2024-07-08 21:17:58 +0300

    making js more clean, separating show/hide card functions

--- js/app.js
+++ js/app.js
@@ -3,36 +3,65 @@
 
 /*---------- Variables (state) ---------*/
 
+let cardsArr = [];
 
 /*----- Cached Element References  -----*/
 
 const startBtn = document.querySelector('button'); //
-const showCard = document.querySelectorAll('.display');
 const hiddenCard = document.querySelectorAll('.hideCard')
-const container = document.querySelector('.container')
+const showCard = document.querySelectorAll('.display');
+// const container = document.querySelector('.container')
 
 /*-------------- Functions -------------*/
 
-const handleClick = () => {
-  showCard.forEach(x => {
-    x.classList.add('showCard');
-    x.classList.remove('hideCard');
-    setTimeout(() => {
-      x.classList.remove('showCard');
-      x.classList.add('hideCard');
-    }, '3000');
-  })
-  // startBtn.disabled = true;
+const showCardElement = (card) => {
+  card.classList.add('showCard');
+  card.classList.remove('hideCard');
 }
-startBtn.disabled = true; // for test remove later
+
+const hideCardElement = (card) => {
+  card.classList.remove('showCard');
+  card.classList.add('hideCard');
+}
+
+const handleClick = () => {
+  showCard.forEach(card => {
+    showCardElement(card);
+
+    setTimeout(() => {
+      hideCardElement(card);
+    }, '5000');
+  })
+  startBtn.disabled = true;
+}
+// startBtn.disabled = true; // for test remove later
 
 const flipCard = (event) => {
-  if (startBtn.disabled == true)
-    {
-      cardIndex = event.target.id;
-      console.log(cardIndex);
-    // hiddenCard[x].classList.remove('hideCard');
-    // hiddenCard[x].classList.add('showCard');
+  if (startBtn.disabled === true && cardsArr.length < 2) {
+    cardIndex = event.target.id;
+    showCardElement(showCard[cardIndex]);
+
+    cardsArr.push(showCard[cardIndex]);
+    if (cardsArr.length === 2)
+      matchingCard();
+  }
+}
+
+const matchingCard = () => {
+  const card1 = cardsArr[0];
+  const card2 = cardsArr[1];
+
+  if (card1.querySelector('img').src !== card2.querySelector('img').src) {
+    setTimeout(() => {
+      hideCardElement(card1);
+      hideCardElement(card2);
+      cardsArr = [];
+    }, '1000');
+    console.log('not match');
+  }
+  else if (card1.querySelector('img').src === card2.querySelector('img').src) {
+    cardsArr = [];
+    console.log('match');
   }
 }
 
